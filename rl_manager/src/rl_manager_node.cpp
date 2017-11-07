a050d5c914b0a48fd88978e9ab9277669a246a76
slzeng <samlzeng@gmail.com>
2017-11-07 01:16:44 -0500

    moving rl_manager to reactive obs. repo

--- rl_manager/src/rl_manager_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <rl_manager/rl_manager.h>
-
-
-int main(int argc, char **argv) {
-    // initialize ros
-    ros::init(argc, argv, "rl_manager_node");
-    RL_manager* rl_man_ptr = new RL_manager();
-    return 0;
-}
